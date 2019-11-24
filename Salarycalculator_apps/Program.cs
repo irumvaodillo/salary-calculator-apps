2c1a483049b9145c8d3daff2da616cdd4bd67140
ipatienceodillo <ipatienceodillo@gmail.com>
2019-11-24 23:32:19 +0300

    git saralyapp first

---
+++ Salarycalculator_apps/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Salarycalculator_apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Empid;
+            string Name;
+            string Position;
+            string Categorie;
+            double NumberOfOur;
+            Console.WriteLine("WELCOME TO SALARY GET");
+            Console.WriteLine("Enter your identification number");
+            Empid = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter your name");
+            Name =(Console.ReadLine());
+            Console.WriteLine("Enter your position");
+            Position =(Console.ReadLine());
+            Console.WriteLine("which categorie are you belong in among this:mothly,hourly or one_time_paid");
+            Categorie =(Console.ReadLine());
+        
+            
+            var Employee1=new SalaryCalculator( Empid,Name,Position,Categorie);
+            if (Categorie=="mothly")
+            {
+                Console.WriteLine("your net salary: " + SalaryCalculator.CalculateSalary(500000));
+            }
+            else if(Categorie=="hourly")
+            {
+                Console.WriteLine("how many hour you did sir?");
+                NumberOfOur = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("your Hourly salary:"+SalaryCalculator.CalculateHourly(3000, NumberOfOur));
+            }
+            else
+            {
+                Console.WriteLine("your Hourly salary:"+ SalaryCalculator.CalculateDaily(3000));
+            }
+           
+           
+        }
+    }
+}
---
+++ Salarycalculator_apps/SalaryCalculator.cs
@@ -0,0 +1,46 @@
+﻿namespace Salarycalculator_apps
+{
+    using System;
+
+    public class SalaryCalculator
+    {
+        public int Empid;
+        public string Name;
+        public string Position;
+        public string Categorie;
+
+        public SalaryCalculator(int empid, string name, string position, string categorie)
+        {
+            this.Empid = empid;
+            this.Name = name;
+            this.Position = position;
+            this.Categorie = categorie;
+        }
+
+        public void test()
+        {
+            System.Console.WriteLine("SalaryCalculator");
+        }
+
+        public static double CalculateSalary(double Gs)
+        {
+            double Salary;
+            Salary = Gs - (Gs * 0.3) - (Gs * 0.05) - (Gs * 0.03);
+            return Salary;
+        }
+
+        public static double CalculateHourly(double RT, double NumberOfOur)
+        {
+            double SalaryHour;
+            SalaryHour = (RT * NumberOfOur) - 0.05 - 0.03;
+            return SalaryHour;
+        }
+
+        public static double CalculateDaily(double Gs)
+        {
+            double SalaryDay;
+            SalaryDay = Gs - (Gs * 0.3);
+            return SalaryDay;
+        }
+    }
+}
